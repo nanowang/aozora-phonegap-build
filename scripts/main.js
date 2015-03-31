24bf2ba61f9ff2c8a001fbe10cfb31b75c394e73
Nano.WANG <namiheike@gmail.com>
2015-03-31 17:46:51 +0800

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,10 @@
 (function() {
+  var onDeviceReady;
 
+  document.addEventListener('deviceready', onDeviceReady, false);
+
+  onDeviceReady = function() {
+    return alert('device ready');
+  };
 
 }).call(this);
